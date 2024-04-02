e528b4f643fbb2de3953842708b28491af8f71b0
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2024-04-02 17:48:04 +0200

    add image_classifier optimizer

---
+++ fashion_classifier/optim.py
@@ -0,0 +1,29 @@
+class Optimizer:
+    """
+    Optimizer for updating model parameters during training.
+
+    It performs a simple gradient descent step by updating the parameters
+    based on their gradients and the specified learning rate (lr).
+    """
+
+    def __init__(self, params, lr):
+        self.params = list(params)
+        self.lr = lr
+
+    def step(self):
+        """
+        Perform a single optimization step by updating the parameters.
+        """
+        for p in self.params:
+            p.data -= p.grad.data * self.lr
+
+    def zero_grad(self):
+        """
+        Reset the gradients of all parameters to zero.
+
+        Since PyTorch accumulates gradients, this method ensures that
+        the gradients from previous optimization steps do not interfere
+        with the current step.
+        """
+        for p in self.params:
+            p.grad = None
